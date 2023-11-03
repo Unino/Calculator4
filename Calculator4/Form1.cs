249ff980c28e9864b7e82ca0862fb01f5319e624
unino <phuongtrang06@gmai.com>
2023-11-03 10:52:35 +0700

    Xu ly dieu kien cua master

--- Calculator4/Form1.cs
+++ Calculator4/Form1.cs
@@ -19,10 +19,13 @@ namespace Calculator4
 
         private void button1_Click(object sender, EventArgs e)
         {
-            double a = double.Parse(textBox1.Text);
-            double b = double.Parse(textBox2.Text);
-            double c = a + b;
-            textBox3.Text = c.ToString();
+            if (textBox1.Text!=String.Empty)
+            {
+                double a = double.Parse(textBox1.Text);
+                double b = double.Parse(textBox2.Text);
+                double c = a + b;
+                textBox3.Text = c.ToString(); 
+            }
         }
 
         private void btTru_Click(object sender, EventArgs e)
